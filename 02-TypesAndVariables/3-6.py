c9411276f316f96da50b76150e53f509817f0e1a
Kamil Pawlowski <s-A121-03@CAMPUS.UEK.KRAKOW.PL>
2025-10-27 12:55:49 +0100

    first_part

---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,10 @@
+import math
+
+sea = 0
+hotel = 20
+
+seaDistance = math.sqrt(sea) * 3.75
+hotelDistance = math.sqrt(hotel) * 3.75
+
+print("Distance to the horizon from sea is: ", seaDistance, " and from hotel is: ", hotelDistance)
+
