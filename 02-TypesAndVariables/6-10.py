8b0fddffa1bab3cd756d58b5b18eec82da8c0d43
Kamil Pawlowski <pawlowski.kamil.to@gmail.com>
2025-11-07 17:01:57 +0100

    update 6

---
+++ 02-TypesAndVariables/6-10.py
@@ -0,0 +1,22 @@
+###
+# String manipulation
+#
+
+movie = "The Lord of the Rings: The Return of the King"
+
+# print number of characters
+print(f'Number of characters: {len(movie)}')
+
+# print title in capital letters
+
+# print title in small letters
+...
+
+# print how many times the vowel "e" appears in the title
+...
+
+# print where in the text is the word "Lord"
+...
+
+# print where in the text is the word "dragon"
+...
---
+++ 02-TypesAndVariables/6-7.py
@@ -0,0 +1,9 @@
+###
+# A program that prints a numerical representation of each letter of your name.
+#
+name = 'Kamil' # replace John with your name
+print(f'The letter {name[0]} has a code {ord(name[0])}')
+print(f'The letter {name[1]} has a code {ord(name[1])}')
+print(f'The letter {name[2]} has a code {ord(name[2])}')
+print(f'The letter {name[3]} has a code {ord(name[3])}')
+print(f'The letter {name[4]} has a code {ord(name[4])}')
