14b36f7e51d06924021a544c569d03154098b075
Kamil Pawlowski <s-A121-04@CAMPUS.UEK.KRAKOW.PL>
2025-11-17 12:55:27 +0100

    EX5

---
+++ 04-Functions/3-1.py
@@ -0,0 +1,13 @@
+###
+# To use the functions available in an external module,
+# you must include that module in your program.
+import math
+
+square_root = math.sqrt(7)
+print('A square root of 7 is', square_root)
+
+natural_logarithm = math.log(5)
+print(f'A natural logarithm of 5 is {natural_logarithm}')
+
+sinus = math.sin(math.radians(90)) #stopnie na radiany -> radiany na sin
+print(f'Sine of 90 degrees is {sinus}')
