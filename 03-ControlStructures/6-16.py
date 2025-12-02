0da5a0f2238670cf6a4b0d756faf3641cc71bb6b
Kamil Pawlowski <pawlowski.kamil.to@gmail.com>
2025-12-02 11:39:29 +0100

    exaam

--- 03-ControlStructures/6-16.py
+++ 03-ControlStructures/6-16.py
@@ -1,8 +1,11 @@
 jacket = 40
 underwear = 70 
 shoes = 20
+rinse = 15
+spin = 9
 
 total_washing_time = 0
+
 program = input('Select washing program: (j)acket, (u)nderwear, (s)hoes: ')
 extra_rinse = input('Extra rinse? (y/n) ')
 extra_spin = input('Extra spin? (y/n) ')
@@ -10,16 +13,23 @@ extra_spin = input('Extra spin? (y/n) ')
 if program == "j":
     total_washing_time =+ jacket
     if extra_rinse == "y":
-        total_washing_time =+ 15
+        total_washing_time = total_washing_time + rinse
     if extra_spin == "y":
-        total_washing_time =+ 9
+        total_washing_time =+ spin
 elif program == "u":
-    total_washing_time =+ underwear
-    
+    total_washing_time = total_washing_time + underwear
+    if extra_rinse == "y":
+        total_washing_time = total_washing_time + rinse
+    if extra_spin == "y":
+        total_washing_time =+ spin
 elif program == "s":
-    total_washing_time =+ shoes
-
+    total_washing_time = total_washing_time + shoes
+    if extra_rinse == "y":
+        total_washing_time = total_washing_time + rinse
+    if extra_spin == "y":
+        total_washing_time =+ spin
 else:
     print("Something went wrong!")
 
 print(f'Total washing time: {total_washing_time} minutes')
+
---
+++ 03-ControlStructures/brudnopis.py
@@ -0,0 +1,11 @@
+generate = "tekst"
+number = str(565656)
+jacket = 40
+underwear = 70 
+shoes = 20
+print(jacket+underwear+shoes)
+
+if number.isdigit():
+    print("generate is digit")
+else:
+    print("generate is not digit")
